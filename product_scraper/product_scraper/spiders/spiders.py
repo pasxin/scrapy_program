6b83df1e936bce4edd57e9d560e061eba310859b
pasxin <iluha1.1000@mail.ru>
2022-08-11 19:03:19 +0300

    2

--- product_scraper/product_scraper/spiders/spiders.py
+++ product_scraper/product_scraper/spiders/spiders.py
@@ -1,20 +1,14 @@
 import scrapy
-
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
 class Price_spiders(CrawlSpider):
     name = "spiders"
     start_urls = ['https://www.perekrestok.ru/cat']
-    rules = (
-        Rule(LinkExtractor(restrict_css=response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get())), 
+    rules =( Rule(LinkExtractor(restrict_css=response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get())), 
         Rule(LinkExtractor(restrict_css=response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get())),
         Rule(LinkExtractor(restrict_css=response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get())), 
-        callback='parce_price'
-    )
-
-
-        
+        callback='parce_price')
+         
         def parce_price (self, response):
             yield {
                 'type':response.css('span.breadcrumb ::text')[3].get(),
