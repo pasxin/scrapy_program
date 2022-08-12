8738506158e631eb6aa976342f44c7ca3292f4f8
pasxin <110281240+pasxin@users.noreply.github.com>
2022-08-12 07:20:30 +0300

    Пунктуация

--- product_scraper/product_scraper/spiders/spiders.py
+++ product_scraper/product_scraper/spiders/spiders.py
@@ -3,11 +3,12 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 class Price_spiders(CrawlSpider):
     name = "spiders"
-    start_urls = ['https://www.perekrestok.ru/cat']
-    rules =( Rule(LinkExtractor(restrict_css=response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
+    start_urls = ['https://www.perekrestok.ru/cat']    rules =
+        ( 
+        Rule(LinkExtractor(restrict_css=response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
         Rule(LinkExtractor(restrict_css=response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
-        Rule(LinkExtractor(restrict_css=response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get())callback='parce_price'), 
-        
+        Rule(LinkExtractor(restrict_css=response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get())callback='parce_price') 
+        )
          
         def parce_price (self, response):
             yield {
