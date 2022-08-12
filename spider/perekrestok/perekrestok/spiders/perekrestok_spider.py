f2198567a8987c728635aa625527db18ba615800
pasxin <Iluha1.1000@mail.ru>
2022-08-12 19:42:48 +0300

    убрал ошибки

--- spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
+++ spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
@@ -3,21 +3,20 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 class Price_spiders(CrawlSpider):
     name = "spiders"
-    start_urls = ['https://www.perekrestok.ru/cat']  
+    start_urls = ['https://www.moscowbooks.ru/']  
 
-  
+         
 
-    rules = 
-        (
-        Rule(LinkExtractor(restrict_css = response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
-        Rule(LinkExtractor(restrict_css = response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
-        Rule(LinkExtractor(restrict_css = response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get()), callback='parce_price')
-        )
+rules = (
+   Rule(LinkExtractor(restrict_css = response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
+   Rule(LinkExtractor(restrict_css = response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
+   Rule(LinkExtractor(restrict_css = response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get()), callback='price')
+   )
         
-     def parce_price (self, response):
-        yield 
-           {
-           'type':response.css('span.breadcrumb ::text')[3].get(),
-           'name':response.css('h1.sc-fubCfw.cqjzZF.product__title ::text').get(),
-           'price':response.css('div.price-new::text').get().split()[0],
-           }
+def price (self, response):
+   yield {}
+   {
+   'type':response.css('span.breadcrumb ::text')[3].get(),
+   'name':response.css('h1.sc-fubCfw.cqjzZF.product__title ::text').get(),
+   'price':response.css('div.price-new::text').get().split()[0],
+   }
