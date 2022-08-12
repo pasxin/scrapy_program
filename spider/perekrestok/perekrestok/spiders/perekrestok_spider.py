6257b64a96d39c0c1e9224e3ad5edc5875bd71d3
pasxin <iluha1.1000@mail.ru>
2022-08-12 17:37:41 +0300

    новое

--- spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
+++ spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
@@ -4,10 +4,14 @@ from scrapy.linkextractors import LinkExtractor
 class Price_spiders(CrawlSpider):
     name = "spiders"
     start_urls = ['https://www.perekrestok.ru/cat']  
-    rules =(Rule(LinkExtractor(restrict_css=response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
-        Rule(LinkExtractor(restrict_css=response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
-        Rule(LinkExtractor(restrict_css=response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get())callback='parce_price'))
-         
+  
+    rules = 
+        (
+        Rule(LinkExtractor(restrict_css = response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
+        Rule(LinkExtractor(restrict_css = response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
+        Rule(LinkExtractor(restrict_css = response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get()), callback='parce_price')
+        )
+        
      def parce_price (self, response):
         yield 
            {
