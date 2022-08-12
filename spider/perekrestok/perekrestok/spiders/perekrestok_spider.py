968cdf256237adbfd0a1b993832c2b2432da20fb
pasxin <Iluha1.1000@mail.ru>
2022-08-12 20:10:54 +0300

    1

--- spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
+++ spider/perekrestok/perekrestok/spiders/perekrestok_spider.py
@@ -3,18 +3,24 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 class Price_spiders(CrawlSpider):
     name = "spiders"
-    start_urls = ['https://www.moscowbooks.ru/']  
+    allowed_domains = ["www.perekrestok.ru"]
+    start_urls = ['www.perekrestok.ru']  
 
          
 
 rules = (
-   Rule(LinkExtractor(restrict_css = response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
-   Rule(LinkExtractor(restrict_css = response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
-   Rule(LinkExtractor(restrict_css = response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get()), callback='price')
-   )
+   Rule(LinkExtractor(allow='/cat/mc/'), follow=True),
+   Rule(LinkExtractor(allow='/p/'), callback='price')
+    )
+
+#rules = (
+   #Rule(LinkExtractor(restrict_css = response.css('div.Box-sc-149qidf-0.dgzgIv a::attr(href)').get()), follow=True), 
+   #Rule(LinkExtractor(restrict_css = response.css('div.sc-kmASHI.bRmtIT a::attr(href)').get()), follow=True),
+   #Rule(LinkExtractor(restrict_css = response.css('div.sc-JAcuL.bEPPNO a::attr(href)').get()), callback='price')
+#   )
         
 def price (self, response):
-   yield {}
+   yield 
    {
    'type':response.css('span.breadcrumb ::text')[3].get(),
    'name':response.css('h1.sc-fubCfw.cqjzZF.product__title ::text').get(),
